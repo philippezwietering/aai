49eee6d132d9f2243b8184c6f90740e0b2036ca9
Philippe Zwietering <philippe.zwietering@gmail.com>
2019-06-20 12:19:19 +0200

    K evaluation function didn't work correctly, evaluated error instead of correctness

--- excercises/knearestneighbors.py
+++ excercises/knearestneighbors.py
@@ -38,6 +38,7 @@ for label in valdates:
 
 daysdata = np.genfromtxt('../resources/days.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5:lambda x: 0 if x == b'-1' else float(x), 7:lambda x: 0 if x == '-1' else float(x)})
 
+# Standard distance function between two vector x and y
 def distance(x, y):
 	help = 0
 	if len(x) != len(y):
@@ -46,12 +47,14 @@ def distance(x, y):
 		help += (y[i]-x[i])**2
 	return math.sqrt(help)
 
+# Calculates the distance between a vector x and a list of vectors data
 def distanceToDataset(x, data):
 	result = []
 	for datapoint in data:
 		result.append(distance(x, datapoint))
 	return result
 
+# Applies the k nearest neighbors algorithm to x for all data and checks with the datalabels
 def knn(k, x, data, datalabels):
 	distances = np.array(distanceToDataset(x, data))
 	indices = distances.argsort()[:k]
@@ -63,27 +66,27 @@ def knn(k, x, data, datalabels):
 	except StatisticsError:
 		return datalabels[distances.argsort()[:1][0]]
 
+# Basically the same as the knn function, only now for a list of xs
 def knn_list(k, xs, data, datalabels):
 	result = []
 	for x in xs:
 		result.append(knn(k, x, data, datalabels))
 	return result
 
+# This function returns the correctness of k for the nearest neighbors algorithm
 def how_good_is_k(k, xs, xslabels, data, datalabels):
 	guesses = knn_list(k, xs, data, datalabels)
 	n = 0
 	for i in range(len(guesses)):
 		if guesses[i] != xslabels[i]:
 			n += 1
-	return n/len(guesses)*100
+	return 100 - n/len(guesses)*100
 
 def get_all_ks(xs, xslabels, data, datalabels):
 	for x in range(len(datalabels)):
 		print(x, how_good_is_k(x, xs, xslabels, data, datalabels))
 
+get_all_ks(valdata, vallabels, data, labels)
 
-
-# get_all_ks(valdata, vallabels, data, labels)
-
-## This returns the classification for the seasons of the days in days.csv with k=5
-print(knn_list(5, daysdata, data, labels))
+## This returns the classification for the seasons of the days in days.csv
+print(knn_list(58, daysdata, data, labels))
